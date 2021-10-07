462a2f7e37ec886ef1b2462fe5f34b337dc84b26
Oleksandr Stovbenko <ostov@softserveinc.com>
2021-10-07 22:11:49 +0300

    Increase spacing

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -1,29 +1,29 @@
 def call(Map config = [:]) {
 
-  // String config.envName
-  // int config.timeOut
-  // int config.lockMilestone
-  // int config.doneMilestone
+    // String config.envName
+    // int config.timeOut
+    // int config.lockMilestone
+    // int config.doneMilestone
 
-  stage("Deploy to ${config.envName}") {
-    options {
-      skipDefaultCheckout()
-      timeout(time: config.timeOut, unit: 'MINUTES')
+    stage("Deploy to ${config.envName}") {
+        options {
+            skipDefaultCheckout()
+            timeout(time: config.timeOut, unit: 'MINUTES')
+        }
+        agent { label config.envName }
+        steps {
+            lock(resource: config.envName, inversePrecedence: true) {
+                milestone config.lockMilestone
+                script { currentBuild.displayName = "${env.APP_VERSION}.${env.APP_BUILD}" }
+                cleanWs()
+                copyArtifacts filter: 'artifact.txt', fingerprintArtifacts: true, projectName: env.JOB_NAME, selector: specific(env.BUILD_NUMBER)
+                echo "Deploying ${env.APP_VERSION}.${env.APP_BUILD} to ${config.envName}"
+                sh '''
+                    echo "[INFO] Deploying ${APP_VERSION}.${APP_BUILD} to ${config.envName} ..."
+                    cat artifact.txt
+                '''
+            }
+            milestone config.doneMilestone
+        }
     }
-    agent { label config.envName }
-    steps {
-      lock(resource: config.envName, inversePrecedence: true) {
-        milestone config.lockMilestone
-        script { currentBuild.displayName = "${env.APP_VERSION}.${env.APP_BUILD}" }
-        cleanWs()
-        copyArtifacts filter: 'artifact.txt', fingerprintArtifacts: true, projectName: env.JOB_NAME, selector: specific(env.BUILD_NUMBER)
-        echo "Deploying ${env.APP_VERSION}.${env.APP_BUILD} to ${config.envName}"
-        sh '''
-          echo "[INFO] Deploying ${APP_VERSION}.${APP_BUILD} to ${config.envName} ..."
-          cat artifact.txt
-        '''
-      }
-      milestone config.doneMilestone
-    }
-  }
 }
--- vars/deploySteps.groovy
+++ vars/deploySteps.groovy
@@ -1,19 +1,19 @@
 def call(Map config = [:]) {
 
-  // String config.envName
-  // int config.lockMilestone
-  // int config.doneMilestone
+    // String config.envName
+    // int config.lockMilestone
+    // int config.doneMilestone
 
-  lock(resource: config.envName, inversePrecedence: true) {
-    milestone 2
-    script { currentBuild.displayName = "${env.APP_VERSION}.${env.APP_BUILD}" }
-    cleanWs()
-    copyArtifacts filter: 'artifact.txt', fingerprintArtifacts: true, projectName: env.JOB_NAME, selector: specific(env.BUILD_NUMBER)
-    echo "Deploying ${env.APP_VERSION}.${env.APP_BUILD} to ${config.envName}"
-    sh '''
-      echo "[INFO] Deploying ${APP_VERSION}.${APP_BUILD} to ${config.envName} ..."
-      cat artifact.txt
-      '''
-  }
-  milestone 3
+    lock(resource: config.envName, inversePrecedence: true) {
+        milestone config.lockMilestone
+        script { currentBuild.displayName = "${env.APP_VERSION}.${env.APP_BUILD}" }
+        cleanWs()
+        copyArtifacts filter: 'artifact.txt', fingerprintArtifacts: true, projectName: env.JOB_NAME, selector: specific(env.BUILD_NUMBER)
+        echo "Deploying ${env.APP_VERSION}.${env.APP_BUILD} to ${config.envName}"
+        sh '''
+            echo "[INFO] Deploying ${APP_VERSION}.${APP_BUILD} to ${config.envName} ..."
+            cat artifact.txt
+        '''
+    }
+    milestone config.doneMilestone
 }
