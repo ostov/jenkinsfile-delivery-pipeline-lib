6206b251a9d40895a487a5f8ad32af4f24a8ad45
Oleksandr Stovbenko <ostov@softserveinc.com>
2021-10-08 01:50:38 +0300

    Use shell script for tests run

--- vars/testSteps.groovy
+++ vars/testSteps.groovy
@@ -9,6 +9,7 @@ def call(Map config = [:]) {
         script {
             currentBuild.displayName = "${env.APP_VERSION}.${env.APP_BUILD}"
             env.ENV_NAME = "${config.envName}"
+            env.TEST_NAME= "${config.testName}"
         }
         /*
         checkout changelog: false, scm: [$class: 'GitSCM',
@@ -22,18 +23,10 @@ def call(Map config = [:]) {
             ]]
         ]
         */
-        echo "Testing on ${config.envName}"
+        echo "Testing ${env.APP_VERSION}.${env.APP_BUILD} on ${config.envName}"
         sh '''
             echo "[INFO] Testing ${APP_VERSION}.${APP_BUILD} on ${ENV_NAME} ..."
+            ./run_tests.sh ${TEST_NAME}
         '''
-        writeFile file: "target/test-results/test/TEST-${config.testName}.xml", text: """
-            <testsuite tests="3">
-                <testcase classname="${config.testName}-1" name="ASuccessfulTest"/>
-                <testcase classname="${config.testName}-2" name="AnotherSuccessfulTest"/>
-                <testcase classname="${config.testName}-3" name="AFailingTest">
-                <failure type="NotEnough${config.testName}"> details about failure </failure>
-                </testcase>
-            </testsuite>
-        """
     }
 }
