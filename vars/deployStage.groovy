f0ba994a13e37f0518957960462b3d2c41591228
Oleksandr Stovbenko <ostov@softserveinc.com>
2021-10-07 21:00:34 +0300

    Add deploySteps.

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -13,17 +13,17 @@ def call(Map config [:]) {
     agent { label config.envName }
     steps {
       lock(resource: config.envName, inversePrecedence: true) {
-        milestone 2
+        milestone config.lockMilestone
         script { currentBuild.displayName = "${env.APP_VERSION}.${env.APP_BUILD}" }
         cleanWs()
         copyArtifacts filter: 'artifact.txt', fingerprintArtifacts: true, projectName: env.JOB_NAME, selector: specific(env.BUILD_NUMBER)
         echo "Deploying ${env.APP_VERSION}.${env.APP_BUILD} to ${config.envName}"
         sh '''
-          echo "[INFO] Deploying to ${config.envName} ..."
+          echo "[INFO] Deploying ${APP_VERSION}.${APP_BUILD} to ${config.envName} ..."
           cat artifact.txt
         '''
       }
-      milestone 3
+      milestone config.doneMilestone
     }
   }
 }
