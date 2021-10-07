77438914d0a2cc35e5feccd4341662fec173f188
Oleksandr Stovbenko <ostov@softserveinc.com>
2021-10-08 02:18:51 +0300

    Add checkout

--- vars/testSteps.groovy
+++ vars/testSteps.groovy
@@ -6,23 +6,22 @@ def call(Map config = [:]) {
 
     lock(resource: config.envName, inversePrecedence: true) {
         milestone config.lockMilestone
-        script {
-            currentBuild.displayName = "${env.APP_VERSION}.${env.APP_BUILD}"
-            env.ENV_NAME = "${config.envName}"
-            env.TEST_NAME= "${config.testName}"
-        }
-        /*
         checkout changelog: false, scm: [$class: 'GitSCM',
             branches: [[name: "refs/heads/master"]],
             doGenerateSubmoduleConfigurations: false,
             extensions: [[$class: 'WipeWorkspace']],
             submoduleCfg: [],
             userRemoteConfigs: [[
-                url: 'https://github.com/ostov/jenkinsfile-delivery-pipeline-tests.git',
+                url: 'https://github.com/ostov/jenkinsfile-delivery-pipeline.git',
                 refspec: '+refs/heads/master:refs/remotes/origin/master',
             ]]
         ]
-        */
+        script {
+            currentBuild.displayName = "${env.APP_VERSION}.${env.APP_BUILD}"
+            env.ENV_NAME = "${config.envName}"
+            env.TEST_NAME= "${config.testName}"
+        }
+
         echo "Testing ${env.APP_VERSION}.${env.APP_BUILD} on ${config.envName}"
         sh '''
             echo "[INFO] Testing ${APP_VERSION}.${APP_BUILD} on ${ENV_NAME} ..."
